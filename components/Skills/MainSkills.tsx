3c91c2d321701501d8961c6ec8a891fd8afee741
hongdakyung <bongbk72@naver.com>
2023-04-26 11:41:24 +0900

    Fix : Main Skill 컴포넌트 수정 - UI 수정(비율) - NEXT.js 내용 추가 by @ddoqi

--- components/Skills/MainSkills.tsx
+++ components/Skills/MainSkills.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MainSkills = () => {
   return (
-    <div className="w-full">
+    <div className="lg:flex items-center justify-center">
       <div>
         <section className="text-gray-600 body-font">
           <div className="flex flex-col text-center w-full mb-[5%]">
@@ -13,9 +13,8 @@ const MainSkills = () => {
               MAIN SKILLS
             </h1>
           </div>
-          <div className="w-full px-5 flex lg:flex-row flex-col">
+          <div className="w-full px-5 flex lg:flex-row flex-col items-center justify-center">
             {/* 메인스킬 */}
-
             <div>
               <div className="main-skills-container">
                 <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
@@ -55,7 +54,7 @@ const MainSkills = () => {
               </div>
               <div className="main-skills-container">
                 <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
-                  <div className="lg:h-[80%] h-full w-1 bg-gray-200 pointer-events-none"></div>
+                  <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
                 </div>
                 <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-blue-500 text-white relative z-10 title-font font-medium text-sm">
                   2
@@ -134,13 +133,14 @@ const MainSkills = () => {
                       1.8s &#8594;0.3s 개선 경험
                       <br /> - SSR , CSR, ISR ,SSG 의 장단점 이해, 상황에 맞는
                       방식을 선택하여 적용
+                      <br />- dynamic import 사용하여 코드 스플리팅
                     </p>
                   </div>
                 </div>
               </div>
               <div className="main-skills-container">
                 <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
-                  <div className="lg:h-[80%] h-full w-1 bg-gray-200 pointer-events-none"></div>
+                  <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
                 </div>
                 <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-blue-500 text-white relative z-10 title-font font-medium text-sm">
                   4
